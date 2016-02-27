1a2da0d4ff68259e758586eac65200ba2c2ea668
unnu <norman.timmler@gmail.com>
2016-02-27 22:21:56 +0100

    Added point2 (not very elegant)

--- lib/dxf/has_point.rb
+++ lib/dxf/has_point.rb
@@ -31,5 +31,51 @@ module DXF
       zd = z - other.z
       Math.sqrt(xd * xd + yd * yd + zd * zd)
     end
+
+    def move(x: nil, y: nil, z: nil)
+      self.x += x if x
+      self.y += y if y
+      self.z += z if z
+      self
+    end
+  end
+
+  module HasPoint2
+    def self.included(base)
+      base.class_eval do
+        field 11, :x2
+        field 21, :y2
+        field 31, :z2
+      end
+    end
+
+    def point2
+      coordinates = [x2, y2, z2].compact
+      if coordinates.empty?
+        Geometry::Point.zero
+      else
+        Geometry::Point[coordinates]
+      end
+    end
+
+    def point2=(point)
+      self.x2 = point.x
+      self.y2 = point.y
+      self.z2 = point.z
+    end
+
+    def distance2(point)
+      xd = x2 - other.x
+      yd = y2 - other.y
+      zd = z2 - other.z
+      Math.sqrt(xd * xd + yd * yd + zd * zd)
+    end
+
+    def move2(x: nil, y: nil, z: nil)
+      self.x2 += x if x
+      self.y2 += y if y
+      self.z2 += z if z
+      self
+    end
   end
 end
