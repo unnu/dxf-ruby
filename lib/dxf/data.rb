16037dcf021727ec0082aa3a3b8d146445aa5e75
unnu <norman.timmler@gmail.com>
2016-03-07 23:54:21 +0100

    Serilaize Arrays

--- lib/dxf/data.rb
+++ lib/dxf/data.rb
@@ -28,17 +28,60 @@ module DXF
     end
 
     def change(marker, code, value)
-      sub_data = @data[marker] ||= []
-      pair = sub_data.find {|pair| pair.first == code}
-      if pair
-        pair[1] = value
+      sub_data = sub_data(marker)
+
+      in_array = false
+      index = nil
+      sub_data.each.with_index do |(_code, _), i|
+        if _code == 102
+          in_array = !in_array
+        else
+          if _code == code && !in_array
+            index = i
+            break
+          end
+        end
+      end
+      index ||= sub_data.size
+
+      if value
+        sub_data[index] = [code, value]
       else
-        @data[marker] << [code, value] if value
+        sub_data.delete_at(index)
+      end
+    end
+
+    def change_array(marker, code, values, array_code, array_name = nil)
+      sub_data = sub_data(marker)
+
+      array_start = sub_data.index {|(_code, _value)| _code == array_code }
+
+      if array_start
+        array_end = sub_data.rindex {|(_code, _value)| _code == array_code }
+      else
+        array_start = array_end = sub_data.size
+      end
+
+      if values.empty?
+        sub_data.slice!(array_start..array_end)
+      else
+        array = []
+        array << [array_code, "{#{array_name}"]
+        array.concat(values.map {|value| [code, value] })
+        array << [array_code, '}']
+
+        sub_data[array_start..array_end] = array
       end
     end
 
     def inspect
       "#<#{self.class.inspect}:#{object_id}>"
+    end
+
+    private
+
+    def sub_data(marker)
+      @data[marker] ||= []
     end
   end
 end
